319298a38a97c98f83261a61b8383e0923dd69fd
Your Name <your.email@csiro.au>
2026-03-27 19:24:55 +1030

    um its a bit of a thinking mess

--- pre_processing_v2/metadata_all_sites.R
+++ pre_processing_v2/metadata_all_sites.R
@@ -26,16 +26,32 @@ suppressPackageStartupMessages({
 })
 
 # ====================== Sites ======================
-#site <- "1.Walpeup_MRS125"
-#site <- "2.Crystal_Brook_Brians_House"
-# site <- "3.Wynarka_Mervs_West"
-# site <- "4.Wharminda"
-# site <- "5.Walpeup_Gums"
- site <- "6.Crystal_Brook_Randals"
+ site <- "1.Walpeup_MRS125"
+ site_number <- "1.Walpeup_MRS125"
+ site_name <- "Walpeup_MRS125"
 
+#site <-"2.Crystal_Brook_Brians_House"
+#site_name <-  "Crystal_Brook_Brians_House"
+
+# site_number <- "3.Wynarka_Mervs_West"
+# site_name <- "Wynarka_Mervs_West"
+
+# site_number <- "4.Wharminda"
+# site_name <- "Wharminda"
+
+# site_number <- "5.Walpeup_Gums"
+# site_name <- "Walpeup_Gums"
+
+# site_number <- "6.Crystal_Brook_Randals"
+# site_name <- "Crystal_Brook_Randals"
 # ====================== PATHS ======================
 readDir <- paste0("//fs1-cbr.nexus.csiro.au/{af-sandysoils-ii}/work/Output-1/", site)
-metadata_path <- "//fs1-cbr.nexus.csiro.au/{lw-soildatarepo}/work/Shiny/Apps/Stirling/GRDCSandySoilsII/Output1Viewer/"
+
+dir <- "//fs1-cbr.nexus.csiro.au/{af-sandysoils-ii}"
+headDir <- paste0(dir, "/work/Output-1/", site_number)
+
+metadata_path <- paste0(dir,"/work/Output-1/0.Site-info/")
+metadata_file_name <- "names of treatments per site 2025 metadata and other info.xlsx"
 
 ## JACKIE - Change the saved directory
 
@@ -49,43 +65,44 @@ saveDir <- paste0("C:/Users/ouz001/working_from_home_post_Sep2022/sandy_soils_ou
 
 file_path_details <- readxl::read_excel(
   paste0(metadata_path,"names of treatments per site 2025 metadata and other info.xlsx"),
-  sheet = "location of file and details") %>% 
+  sheet = "file location etc") %>% 
   filter(Site == site)
 
 seasons <- readxl::read_excel(
   paste0(metadata_path,"names of treatments per site 2025 metadata and other info.xlsx"),
   sheet = "seasons") %>% 
-  filter(Site == site)
+  filter(Site == site) %>% 
+  filter(Year == 2025)
+
+
+soil_path <- file_path_details %>%  
+  filter(variable == "location of key soil tif") %>% pull("file path")
+zone_shapefile_path <- 
+  file_path_details %>%  
+  filter(variable == "location of zone shp") %>% pull("file path")
+zone_raster_path <- file_path_details %>%  
+  filter(variable == "location of zone tif") %>% pull("file path")
+bounary_shapefile_path <- file_path_details %>%  
+  filter(variable == "boundary") %>% pull("file path")
+trial.plan_shapefile_path <- file_path_details %>%  
+  filter(variable == "trial.plan") %>% pull("file path")
 
 
 ################################################################################
-######################## 1) Read in Paddock maps ###############################
+######################## 1) Read in Paddock maps and info ######################
 ################################################################################
 
-soil <- rast(file.path(readDir, file_path_details$`location of key soil tif`))
 
-#names(soil) <- c("Surface_pH","Subsoil_Clay_pct","Repellence","DepthToFizz","DepthToClay","DepthToNeutral")
-## Not sure I need this??
+soil <- rast(file.path(readDir, soil_path))
+zones <- rast(file.path(readDir, zone_raster_path))
 
-
-zones.sf <- st_read(paste0(readDir,file_path_details$`location of zone shp`))
-zones <- rast(file.path(readDir, file_path_details$`location of zone tif`))
-
-
+zones.sf <- st_read(paste0(readDir,zone_shapefile_path))
+boundary   <- suppressMessages(st_read(file.path(readDir, bounary_shapefile_path), quiet = TRUE))
+trial.plan <- suppressMessages(st_read(file.path(readDir, trial.plan_shapefile_path), quiet = TRUE))
 
 ################################################################################
 ######################## 2) Paddock Information  ###############################
 ################################################################################
-
-boundary   <- suppressMessages(st_read(file.path(readDir, file_path_details$boundary), quiet = TRUE))
-trial.plan <- suppressMessages(st_read(file.path(readDir, file_path_details$trial.plan), quiet = TRUE))
-
-
-
-# --- minimal CRS checks (good) ---
-# stopifnot(st_crs(boundary)$epsg == 4326)
-# stopifnot(st_crs(trial.plan)$epsg == 4326)
-# stopifnot("treat_desc" %in% names(trial.plan))
 
 # --- assemble named bundle ---
 site.info <- list(
@@ -102,12 +119,14 @@ class(site.info) <- c("ssii_site", class(site.info))
 validate_site <- function(x) {
   stopifnot(inherits(x, "ssii_site"))
   stopifnot(all(c("site_id","boundary","trial_plan","seasons", "zones", "zones_sf") %in% names(x)))
-  stopifnot(st_crs(x$boundary)$epsg == 4326, st_crs(x$trial_plan)$epsg == 4326)
+  #stopifnot(st_crs(x$boundary)$epsg == 4326, st_crs(x$trial_plan)$epsg == 4326)
   stopifnot(is.double(x$seasons$year))
   invisible(x)
 }
 validate_site(site.info)
 
 # --- save ---
-saveRDS(site.info, file.path(saveDir, "site_info.rds"))
+#saveRDS(site.info, file.path(saveDir, "site_info.rds"))
+headDir_Jaxs <- paste0(headDir,"/Jackie")
+saveRDS(site.info, file.path(headDir_Jaxs, "site_info.rds"))
 
